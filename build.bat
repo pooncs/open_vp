d7274272a8cffaac18722f9357eeb3a80af13d56
sam.poon <sam.poon@hmgics.com>
2025-12-12 17:18:45 +0800

    refactor(build): restructure cmake configuration for 3rdparty dependencies
    - Add 3rdParty directory to gitignore
    - Update build.bat to use new build directory naming
    - Disable AUTOMOC and implement manual MOC processing
    - Update Qt and QtNodes paths to use local 3rdParty copies
    - Simplify build by temporarily disabling tests
    - Add post-build commands to copy required DLLs

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-cmake -S . -B build -G "Visual Studio 16 2019" -A x64
-cmake --build build --config Release
+cmake -S . -B build_vs2019_x64 -G "Visual Studio 16 2019" -A x64
+cmake --build build_vs2019_x64 --config Release
